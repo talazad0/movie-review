51d3006b3d4da1eec9be5ca4b5e61c34a73fa1ac
Timothy AlAzawie <t.alazawie@yahoo.co.uk>
2023-12-06 15:37:33 +0000

    started seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,43 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require "json"
-require "open-uri"
+require "uri"
+require "net/http"
 
-puts "Cleaning database"
-Movie.destroy_all
+Movie.delete_all
 
 puts "Creating Movies"
 
+url = URI("https://netflix54.p.rapidapi.com/search/?query=movie&offset=0&limit_titles=50&limit_suggestions=20&lang=en")
 
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
 
-10.times do
-  Movie.create!(
-    name: Faker::Movie.title,
-    synopsis: Faker::Movie.quote
-  )
+# Create an HTTP request
+request = Net::HTTP::Get.new(url)
+request["X-RapidAPI-Key"] = '48b6192f7amshb5e2a948637df63p1d867ajsne93026296e26'
+request["X-RapidAPI-Host"] = 'netflix54.p.rapidapi.com'
+
+# Send the request and get the response
+response = http.request(request)
+
+# Parse the JSON response
+data = JSON.parse(response.read_body)
+
+# Assuming you have a Movie model with 'title' and 'synopsis' attributes
+movies = data['titles'].map do |title_info|
+  name = title_info.dig('jawSummary', 'maturity', 'title')
+  synopsis = title_info.dig('jawSummary', 'maturity', 'synopsis')
+
+  {
+    name: name,
+    synopsis: synopsis
+  }
+end.compact # Remove nil entries
+
+# Seed the database
+movies.each do |movie|
+  Movie.create!(name: movie[:name], synopsis: movie[:synopsis])
 end
 
 puts "Finished"
