adfbf018eeb06381fab280fc123cf82b7668d46e
Timothy AlAzawie <t.alazawie@yahoo.co.uk>
2023-12-12 20:58:27 +0000

    finished seed, fixed nav/ home

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root to: "pages#home"
+  root to: "movies#index"
 
   resources :movies
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,36 +13,55 @@ Movie.delete_all
 
 puts "Creating Movies"
 
-url = URI("https://netflix54.p.rapidapi.com/search/?query=movie&offset=0&limit_titles=50&limit_suggestions=20&lang=en")
+#url = URI("https://netflix54.p.rapidapi.com/search/?query=movie&offset=0&limit_titles=50&limit_suggestions=20&lang=en")
+url = URI("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc")
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
 
 # Create an HTTP request
 request = Net::HTTP::Get.new(url)
-request["X-RapidAPI-Key"] = '48b6192f7amshb5e2a948637df63p1d867ajsne93026296e26'
-request["X-RapidAPI-Host"] = 'netflix54.p.rapidapi.com'
+#request["X-RapidAPI-Key"] = '48b6192f7amshb5e2a948637df63p1d867ajsne93026296e26'
+#request["X-RapidAPI-Host"] = 'netflix54.p.rapidapi.com'
+request["accept"] = 'application/json'
+request["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMGQyZGJjZGVjYTg0YWNjMGU0MDc3YTdhZGFjZWQzOSIsInN1YiI6IjY1Nzc2NWFiMjBlY2FmMDEzYWNiMmExYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h6a6mdHzsSSVh_IZMh6sgLUPXatkJVZadrOy9JtDR9c'
 
-# Send the request and get the response
 response = http.request(request)
-
-# Parse the JSON response
 data = JSON.parse(response.read_body)
 
-# Assuming you have a Movie model with 'title' and 'synopsis' attributes
-movies = data['titles'].map do |title_info|
-  name = title_info.dig('jawSummary', 'maturity', 'title')
-  synopsis = title_info.dig('jawSummary', 'maturity', 'synopsis')
+#movies = data['titles'].map do |title_info|
+#  name = title_info.dig('jawSummary', 'maturity', 'title')
+#  synopsis = title_info.dig('jawSummary', 'maturity', 'synopsis')
 
-  {
-    name: name,
-    synopsis: synopsis
-  }
-end.compact # Remove nil entries
+  #{
+ #   name: name,
+#    synopsis: synopsis
+#  }
+#end.compact
 
-# Seed the database
-movies.each do |movie|
-  Movie.create!(name: movie[:name], synopsis: movie[:synopsis])
+
+#movies.each do |movie|
+#  Movie.create!(name: movie[:name], synopsis: movie[:synopsis])
+#end
+
+#10.times do
+#  Movie.create!(
+#    name: data['title_info']['jawSummary']['maturity']['title'],
+#    synopsis: data['title-info']['jawSummary']['maturity']['synopsis']
+#  )
+#end
+
+data['results'].first(20).each do |movie_data|
+  name = movie_data['title'] # Assuming the API response contains 'title' field for the movie name
+  synopsis = movie_data['overview']
+
+  begin
+    Movie.create!(name: name, synopsis: synopsis)
+    puts "Successfully created: #{name}"
+  rescue StandardError => e
+    puts "Failed to create: #{name}"
+    puts "Error: #{e.message}"
+  end
 end
 
 puts "Finished"
